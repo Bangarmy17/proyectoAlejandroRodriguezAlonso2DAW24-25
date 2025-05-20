9254ec7aab5193125d1d15b54e332199b8d89b27
Alex Rodríguez Alonso <78348488+Bangarmy17@users.noreply.github.com>
2025-05-20 20:33:42 +0200

    Delete proyectoBack/ApiRestBackEnd/ApiRestBackEnd directory

--- proyectoBack/ApiRestBackEnd/ApiRestBackEnd/src/main/java/com/dwes/ApiRestBackEnd/repository/RealizarPedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dwes.ApiRestBackEnd.repository;
-
-import com.dwes.ApiRestBackEnd.model.RealizarPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RealizarPedidoRepository extends JpaRepository<RealizarPedido, Long> {
-}
